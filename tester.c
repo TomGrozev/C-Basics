475ca4ac58831019b870dc0c3af5396e65da4b3a
TomGrozev <1491414+TomGrozev@users.noreply.github.com>
2021-04-18 20:12:42 +1000

    Improved output logging

--- tester.c
+++ tester.c
@@ -21,6 +21,7 @@ int main() {
     create(&vector);
 
     // Show the vector is empty
+    printf("\nEmpty Vector: \n");
     printVector(vector);
 
     // Add integers to the vector
@@ -29,10 +30,11 @@ int main() {
     add(&vector, 9);
 
     // Show the integers in the vector
+    printf("\nAdded elements: \n");
     printVector(vector);
 
     // Print all elements in vector using elementAt function
-    printf("\nPrinting all elements: \n");
+    printf("\nPrinting all elements using elementAt(): \n");
     for (int i = 0; i < vector.size; ++i) {
         printf("Element at %d: %d \n", i, elementAt(&vector, i));
     }
@@ -42,18 +44,21 @@ int main() {
     add(&vector, 6);
 
     // Show the integers in the vector
+    printf("\nAdded more elements: \n");
     printVector(vector);
 
     // Remove all 6 integers from the vector
     removeElements(&vector, 6);
 
     // Show the 6 elements have been removed
+    printf("\nRemoved all 6 values: \n");
     printVector(vector);
 
     // Trim the vector capacity to match the size
     trimToSize(&vector);
 
     // Show the trimmed vector
+    printf("\nTrimmed vector: \n");
     printVector(vector);
 
     return 0;
